4905a15c66852adcb8a172de2649c75c5a61d8ad
Anmol <anmol.volt@gmail.com>
2023-04-09 17:13:52 +0530

    $

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
 require('dotenv').config()
-const { REST, Routes } = require('discord.js');
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-const commands = [
-  {
-    name: 'ping',
-    description: 'Replies with Pong!',
-  },
-];
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
 
-const rest = new REST({ version: '10' }).setToken(TOKEN);
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isChatInputCommand()) return;
 
-(async () => {
-  try {
-    console.log('Started refreshing application (/) commands.');
-
-    await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
-
-    console.log('Successfully reloaded application (/) commands.');
-  } catch (error) {
-    console.error(error);
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
   }
-})();
+});
+
+client.login(TOKEN);
