e5f47f15e21f6c21771ca053deb4c54cf300562f
Anmol <anmol.volt@gmail.com>
2023-04-09 23:36:48 +0530

    3

--- register-commands.js
+++ register-commands.js
@@ -1,11 +1,24 @@
 require('dotenv').config();
-const { REST, Routes } = require('discord.js');
+const { REST, Routes, Options, Application, ApplicationCommandOptionType } = require('discord.js');
 
 const commands = [
   {
     name: 'ping',
     description: 'Replies with Pong!',
   },
+
+  {
+    name: "pokemon",
+    description: "details of Pokemon",
+    options: [
+        {
+            name:"pokemon-name",
+            description: "Search Pokemon with name",
+            type: ApplicationCommandOptionType.String,
+            required: true,
+        }
+    ]
+  }
 ];
 
 const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
