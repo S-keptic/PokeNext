12c3de66fa98471dd276b32ca89affb29fbf2e03
Skeptic <69573771+S-keptic@users.noreply.github.com>
2023-04-18 14:37:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { Client, GatewayIntentBits } = require('discord.js');
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`); 
+  console.log(`Logged in as ${client.user.tag}!`);  
 });
 
 client.on('interactionCreate', async interaction => {
